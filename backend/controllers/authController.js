69bc108304e8296e7f548aa43eb80bef262fc849
ravi <ravindra@stavyah.com>
2025-04-10 17:02:04 +0530

    auth controller samesite

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -41,13 +41,13 @@ export const loginUser = async (req, res) => {
     .cookie('token', accessToken, {
       httpOnly: true,
       secure: true,
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 15 * 60 * 1000, // 15 mins
     })
     .cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     })
     .json({
