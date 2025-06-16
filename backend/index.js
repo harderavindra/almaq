38aa25c886fd062e891bd93760348440c887940c
ravi <ravindra@stavyah.com>
2025-06-16 16:57:08 +0530

    fix(cors): add additional origins for improved API accessibility

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['http://localhost:5174','http://localhost:5173', 'https://almaq-frontend.vercel.app'],
+    origin: ['http://localhost:5174','http://localhost:5173', 'https://almaq-frontend.vercel.app',
+         'https://almaqbiotech.com',
+  'https://www.almaqbiotech.com',
+  'https://api.almaqbiotech.com',
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH','DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
     credentials: true,
